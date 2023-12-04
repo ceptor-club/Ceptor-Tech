9c8206604ddde650c3176bc64a1b084b5f0cc1de
Emanherawy <eman.herawy@gmail.com>
2023-12-04 12:51:00 +0300

    fmt

--- smart_contracts/src/Ceptor.sol
+++ smart_contracts/src/Ceptor.sol
@@ -7,141 +7,121 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
- 
- 
-interface FunctionInterface {
-  //used for calling functions from other contracts
-  function balanceOf(address account, uint256 id)
-    external
-    view
-    returns (uint256);
 
-  function burn(
-    address,
-    uint256 id,
-    uint256 amount
-  ) external;
+interface FunctionInterface {
+    //used for calling functions from other contracts
+    function balanceOf(address account, uint256 id) external view returns (uint256);
+
+    function burn(address, uint256 id, uint256 amount) external;
 }
 
-contract Ceptors is
-  ERC721,
-  ERC721Enumerable,
-  ERC721URIStorage,
-  Pausable,
-  ERC721Burnable
-{
- 
-  uint256  _tokenIdCounter;
+contract Ceptors is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, ERC721Burnable {
+    uint256 _tokenIdCounter;
 
-  address public diceContractAddress;
+    address public diceContractAddress;
 
-  mapping(address => uint256) public userTimers;
+    mapping(address => uint256) public userTimers;
 
-  constructor(address _diceAddress) ERC721("Ceptors", "CPTR") {
-    _grantRole(OWNER, msg.sender);
-    _grantRole(MINTER, msg.sender);
-    _grantRole(MINTER, _diceAddress);
-    diceContractAddress = _diceAddress;
-  }
-
-  function setPause(bool _isPaused) public onlyRole(OWNER) {
-    if (_isPaused) {
-      _pause();
-    } else {
-      _unpause();
+    constructor(address _diceAddress) ERC721("Ceptors", "CPTR") {
+        _grantRole(OWNER, msg.sender);
+        _grantRole(MINTER, msg.sender);
+        _grantRole(MINTER, _diceAddress);
+        diceContractAddress = _diceAddress;
     }
-  }
-      function _increaseBalance(address account, uint128 value) internal override (ERC721Enumerable,ERC721) {
-       super._increaseBalance(account, value);
+
+    function setPause(bool _isPaused) public onlyRole(OWNER) {
+        if (_isPaused) {
+            _pause();
+        } else {
+            _unpause();
+        }
     }
- function _update(address to, uint256 tokenId, address auth) internal virtual override (ERC721Enumerable,ERC721)
-  returns (address) {
- return super._update(to, tokenId, auth);
- }
-  // function unpause() public onlyRole(OWNER) {
-  //   _unpause();
-  // }
 
-  function mint(string memory uri) public whenNotPaused {
-    require(
-      isBefore(userTimers[msg.sender]),
-      "Time is up! Insert another Dize to get more time."
-    );
-
-    uint256 tokenId = _tokenIdCounter ;
-    userTimers[msg.sender] = 0;
-    _tokenIdCounter++;
-    _safeMint(msg.sender, tokenId);
-    _setTokenURI(tokenId, uri);
-  }
-
-  function isBefore(uint256 _userTimer) private view returns (bool) {
-    return block.timestamp < _userTimer;
-  }
-
-  function setTimer(address _address, uint256 _time) public onlyRole(MINTER) {
-    if (userTimers[_address] == 0 || !isBefore(userTimers[_address])) {
-       userTimers[_address] = block.timestamp + _time;
-    } else {
-      userTimers[_address] += _time;
+    function _increaseBalance(address account, uint128 value) internal override(ERC721Enumerable, ERC721) {
+        super._increaseBalance(account, value);
     }
-  }
 
-  // The following functions are overrides required by Solidity.
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        virtual
+        override(ERC721Enumerable, ERC721)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+    // function unpause() public onlyRole(OWNER) {
+    //   _unpause();
+    // }
 
- 
- 
+    function mint(string memory uri) public whenNotPaused {
+        require(isBefore(userTimers[msg.sender]), "Time is up! Insert another Dize to get more time.");
 
-  function tokenURI(uint256 tokenId)
-    public
-    view
-    override(ERC721, ERC721URIStorage)
-    returns (string memory)
-  {
-    return super.tokenURI(tokenId);
-  }
+        uint256 tokenId = _tokenIdCounter;
+        userTimers[msg.sender] = 0;
+        _tokenIdCounter++;
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
 
-  function supportsInterface(bytes4 interfaceId)
-    public
-    view
-    override(ERC721, ERC721Enumerable, ERC721URIStorage)
-    returns (bool)
-  {
-    return super.supportsInterface(interfaceId);
-  }
+    function isBefore(uint256 _userTimer) private view returns (bool) {
+        return block.timestamp < _userTimer;
+    }
 
-  // ACCESS CONTROL ----------------------------
+    function setTimer(address _address, uint256 _time) public onlyRole(MINTER) {
+        if (userTimers[_address] == 0 || !isBefore(userTimers[_address])) {
+            userTimers[_address] = block.timestamp + _time;
+        } else {
+            userTimers[_address] += _time;
+        }
+    }
 
-  mapping(bytes32 => mapping(address => bool)) public roles;
-  //0xf0887ba65ee2024ea881d91b74c2450ef19e1557f03bed3ea9f16b037cbe2dc9
-  bytes32 public constant MINTER = keccak256(abi.encodePacked("MINTER")); // minter role is for other contracts to mint tokens
-  bytes32 public constant OWNER = keccak256(abi.encodePacked("OWNER")); // owner role is for the owner of the contract
+    // The following functions are overrides required by Solidity.
 
-  modifier onlyRole(bytes32 role) {
-    require(roles[role][msg.sender], "Not authorized.");
-    _;
-  }
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
 
-  function _grantRole(bytes32 role, address account) internal {
-    roles[role][account] = true;
-  }
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 
-  function grantRole(bytes32 role, address account) external onlyRole(OWNER) {
-    _grantRole(role, account);
-  }
+    // ACCESS CONTROL ----------------------------
 
-  function _revokeRole(bytes32 role, address account) internal {
-    roles[role][account] = false;
-  }
+    mapping(bytes32 => mapping(address => bool)) public roles;
+    //0xf0887ba65ee2024ea881d91b74c2450ef19e1557f03bed3ea9f16b037cbe2dc9
+    bytes32 public constant MINTER = keccak256(abi.encodePacked("MINTER")); // minter role is for other contracts to mint tokens
+    bytes32 public constant OWNER = keccak256(abi.encodePacked("OWNER")); // owner role is for the owner of the contract
 
-  function revokeRole(bytes32 role, address account) external onlyRole(OWNER) {
-    _revokeRole(role, account);
-  }
+    modifier onlyRole(bytes32 role) {
+        require(roles[role][msg.sender], "Not authorized.");
+        _;
+    }
 
-  function transferOwnership(address newOwner) external onlyRole(OWNER) {
-    _grantRole(OWNER, newOwner);
-    _grantRole(MINTER, newOwner);
-    _revokeRole(OWNER, msg.sender);
-    _revokeRole(MINTER, msg.sender);
-  }
+    function _grantRole(bytes32 role, address account) internal {
+        roles[role][account] = true;
+    }
+
+    function grantRole(bytes32 role, address account) external onlyRole(OWNER) {
+        _grantRole(role, account);
+    }
+
+    function _revokeRole(bytes32 role, address account) internal {
+        roles[role][account] = false;
+    }
+
+    function revokeRole(bytes32 role, address account) external onlyRole(OWNER) {
+        _revokeRole(role, account);
+    }
+
+    function transferOwnership(address newOwner) external onlyRole(OWNER) {
+        _grantRole(OWNER, newOwner);
+        _grantRole(MINTER, newOwner);
+        _revokeRole(OWNER, msg.sender);
+        _revokeRole(MINTER, msg.sender);
+    }
 }
--- smart_contracts/src/Prompt.sol
+++ smart_contracts/src/Prompt.sol
@@ -9,98 +9,98 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 
 contract Prompt is VRFConsumerBaseV2, Ownable {
-  VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
-  bytes32 private immutable i_keyhash; //gas Lane
-  uint64 private immutable i_subscriptionId;
-  uint16 private constant REQUEST_CONFIRMATIONS = 3;
-  uint32 private immutable i_callbackGasLimit;
-  uint32 private constant NUM_WORDS = 1;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_keyhash; //gas Lane
+    uint64 private immutable i_subscriptionId;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private constant NUM_WORDS = 1;
 
-  string[10] private i_prompts = [
-    "Prompt 1",
-    "Prompt 2",
-    "Prompt 3",
-    "Prompt 4",
-    "Prompt 5",
-    "Prompt 6",
-    "Prompt 7",
-    "Prompt 8",
-    "Prompt 9",
-    "Prompt 10"
-  ];
+    string[10] private i_prompts = [
+        "Prompt 1",
+        "Prompt 2",
+        "Prompt 3",
+        "Prompt 4",
+        "Prompt 5",
+        "Prompt 6",
+        "Prompt 7",
+        "Prompt 8",
+        "Prompt 9",
+        "Prompt 10"
+    ];
 
-  string public s_currentPrompt;
-  uint256 public lastUpdated;
-  string private s_lastPrompt;
-  string private s_secondLastPrompt;
-  uint public lastTimeStamp;
+    string public s_currentPrompt;
+    uint256 public lastUpdated;
+    string private s_lastPrompt;
+    string private s_secondLastPrompt;
+    uint256 public lastTimeStamp;
 
-  event CurrentPrompt(string _currentPrompt);
-  constructor(
-    address new_owner,
-    address vrfCoordindatorV2,
-    bytes32 keyhash /* gasLane */,
-    uint64 subscriptionId,
-    uint32 callbackGasLimit
-  ) VRFConsumerBaseV2(vrfCoordindatorV2) Ownable(new_owner) {
-    i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordindatorV2);
-    i_keyhash = keyhash;
-    i_subscriptionId = subscriptionId;
-    i_callbackGasLimit = callbackGasLimit;
-  }
+    event CurrentPrompt(string _currentPrompt);
 
-
-  function requestPromptUpdate() public returns (uint256 requestId) {
-    requestId = i_vrfCoordinator.requestRandomWords(
-      i_keyhash /* Or gasLane once again */,
-      i_subscriptionId,
-      REQUEST_CONFIRMATIONS,
-      i_callbackGasLimit,
-      NUM_WORDS
-    );
-  }
-
-  function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
-    // Find a new prompt that hasn't been used in the last two updates
-    string memory newPrompt;
-    uint256 i = randomWords[0] % i_prompts.length;
-    while (compareStrings(i_prompts[i], s_lastPrompt) || compareStrings(i_prompts[i], s_secondLastPrompt)) {
-      if (i == i_prompts.length - 1) {
-        i = 0; // reset if we have checked all prompts
-      } else {
-        i++; // move to the next prompt
-      }
+    constructor(
+        address new_owner,
+        address vrfCoordindatorV2,
+        bytes32 keyhash, /* gasLane */
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordindatorV2) Ownable(new_owner) {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordindatorV2);
+        i_keyhash = keyhash;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
-    newPrompt = i_prompts[i];
 
-    // Update the last two used prompts
-    s_secondLastPrompt = s_lastPrompt;
-    s_lastPrompt = newPrompt;
+    function requestPromptUpdate() public returns (uint256 requestId) {
+        requestId = i_vrfCoordinator.requestRandomWords(
+            i_keyhash, /* Or gasLane once again */
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+    }
 
-    // Set the current prompt to the new prompt
-    s_currentPrompt = newPrompt;
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        // Find a new prompt that hasn't been used in the last two updates
+        string memory newPrompt;
+        uint256 i = randomWords[0] % i_prompts.length;
+        while (compareStrings(i_prompts[i], s_lastPrompt) || compareStrings(i_prompts[i], s_secondLastPrompt)) {
+            if (i == i_prompts.length - 1) {
+                i = 0; // reset if we have checked all prompts
+            } else {
+                i++; // move to the next prompt
+            }
+        }
+        newPrompt = i_prompts[i];
 
-    emit CurrentPrompt(s_currentPrompt);
-  }
+        // Update the last two used prompts
+        s_secondLastPrompt = s_lastPrompt;
+        s_lastPrompt = newPrompt;
 
-  // Helper function to compare strings
-  function compareStrings(string memory a, string memory b) private pure returns (bool) {
-    return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
-  }
+        // Set the current prompt to the new prompt
+        s_currentPrompt = newPrompt;
 
-  function getPrompt(uint8 index) public view returns (string memory) {
-    return i_prompts[index];
-  }
+        emit CurrentPrompt(s_currentPrompt);
+    }
 
-  function getTotalPromptCount() public view returns (uint256) {
-    return uint256(i_prompts.length);
-  }
+    // Helper function to compare strings
+    function compareStrings(string memory a, string memory b) private pure returns (bool) {
+        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+    }
 
-  function getCurrentPrompt() public view returns (string memory) {
-    return s_currentPrompt;
-  }
+    function getPrompt(uint8 index) public view returns (string memory) {
+        return i_prompts[index];
+    }
 
-  function setNewTenPrompts(string[10] memory newPrompts) external onlyOwner {
-    i_prompts = newPrompts;
-  }
+    function getTotalPromptCount() public view returns (uint256) {
+        return uint256(i_prompts.length);
+    }
+
+    function getCurrentPrompt() public view returns (string memory) {
+        return s_currentPrompt;
+    }
+
+    function setNewTenPrompts(string[10] memory newPrompts) external onlyOwner {
+        i_prompts = newPrompts;
+    }
 }
