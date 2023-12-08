30d7fdb7847b20ae66f9b850fa952c4f704242ec
Lena Hierzi <lenahierzi@hotmail.com>
2023-12-08 10:08:37 +0000

    add addresses fle for all contract addresses

---
+++ frontend/utils/addresses.ts
@@ -0,0 +1,37 @@
+export const addresses = {
+  sepolia: {
+    prompt: "",
+    promtCollection: "",
+    ceptorDice: "0x7b958df42778255643734f4b3ea81d2e2c2b5195",
+    ceptors: "",
+    ccipV4: "",
+  },
+  goerli: {
+    prompt: "",
+    promtCollection: "",
+    ceptorDice: "",
+    ceptors: "",
+    ccipV4: "",
+  },
+  mumbai: {
+    prompt: "",
+    promtCollection: "",
+    ceptorDice: "",
+    ceptors: "",
+    ccipV4: "",
+  },
+  polygonZkEVM: {
+    prompt: "",
+    promtCollection: "",
+    ceptorDice: "",
+    ceptors: "",
+    ccipV4: "",
+  },
+  fuji: {
+    prompt: "",
+    promtCollection: "",
+    ceptorDice: "",
+    ceptors: "",
+    ccipV4: "",
+  },
+};
