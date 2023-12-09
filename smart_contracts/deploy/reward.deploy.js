f701161342d22b23447ab47632c32861c85329aa
Emanherawy <eman.herawy@gmail.com>
2023-12-09 04:48:11 +0300

    add hardhat support to be able to deploy Reward.sol

---
+++ smart_contracts/deploy/reward.deploy.js
@@ -0,0 +1,28 @@
+// const { parseEther } = require('ethers/lib/utils')
+const fs = require('fs').promises;  // Import 'promises' from 'fs' module
+
+// deploy/00_deploy_my_contract.js
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, get, execute } = deployments
+    const { deployer } = await getNamedAccounts()
+    let prompt =  "0x425c05E479C94227f6CeBB52aF899330B542de88"
+    let dice =  "0x71B80d94645ddF04BAa56d8D3c92792496A23B27"
+    const oracle = "0xA9d587a00A31A52Ed70D6026794a8FC5E2F5dCb0";
+    const subscriptionId = 20;
+    const _checkLogic = await fs.readFile("./source.js", "utf8");
+
+   
+    // variables go here 
+    // const _owner = deployer// 0xac701BB1557F6c06070Bc114Ca6Ace4a358D3A84
+     await deploy('Reward', {
+        from: deployer,
+        args: [
+
+
+            oracle, subscriptionId, _checkLogic, dice, prompt
+        ],
+        log: true,
+    })
+
+   }
+module.exports.tags = ['Reward']
---
+++ smart_contracts/source.js
@@ -0,0 +1,33 @@
+const weekTimestamp = args[0];
+// Add your API key here
+const apiKey = "testKey";
+
+const headers = {
+    Accept: "application/json;",
+    apiKey: "testkey", // Add your API key here
+};
+const apiResponse = await Functions.makeHttpRequest({
+    url: 'https://ceptor-backend-staging.up.railway.app/mostLikedSubmission',
+    headers: headers,
+});
+
+console.log('API response:', JSON.stringify(apiResponse, null, 2));
+
+if (apiResponse.error) {
+    console.error(apiResponse.error);
+    throw Error("Request failed");
+}
+
+const { data } = apiResponse;
+
+console.log("API response data:", JSON.stringify(data, null, 2));
+
+const { tokenID } = data;
+
+if (!tokenID) {
+    throw new Error('No token ID found in response:'+ JSON.stringify(data));
+}
+
+return Functions.encodeString(JSON.stringify({
+    tokenID
+}));
