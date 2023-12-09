b7705a2fdae719fd7e3d301b15230e83b5d3cdbc
Emanherawy <eman.herawy@gmail.com>
2023-12-09 04:48:11 +0300

    add hardhat support to be able to deploy Reward.sol

---
+++ smart_contracts/hardhat.config.js
@@ -0,0 +1,35 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-foundry");
+/**import 'hardhat-deploy';
+import 'hardhat-deploy-ethers'; */
+require("hardhat-deploy")
+require("hardhat-deploy-ethers")
+const  dotenv = require("dotenv");
+dotenv.config();
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  namedAccounts: {
+    deployer: 0,
+  },
+  solidity: "0.8.20",
+  networks: {
+   
+    mumbai: {
+      url: 'https://polygon-mumbai.g.alchemy.com/v2/tCbwTAqlofFnmbVORepuHNcsrjNXWdRJ',
+      accounts: [process.env.PRIVATE_KEY || ''],
+      saveDeployments: true,
+    },
+    fuji: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      accounts: [process.env.PRIVATE_KEY || ''],
+      saveDeployments: true,
+    },
+  },
+  paths: {
+    sources: './src',
+    deploy: 'deploy',
+    deployments: 'deployments',
+    tests: './test',
+    cache: './cache',
+   },
+};
