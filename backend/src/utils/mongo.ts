fb9bfffd97e63dbe3bfeece49a8cb5968cc60365
Alan Barry <alanklinect@gmail.com>
2023-12-09 12:26:38 -0600

    trying to trouble shoot

--- backend/src/utils/mongo.ts
+++ backend/src/utils/mongo.ts
@@ -1,12 +1,21 @@
 import { MongoClient } from "mongodb";
 require("dotenv").config();
 
+const url = process.env.DB_CONN_STRING as string
+const client = new MongoClient(url)
+
 interface User {
   _id: string;
   name: string;
   email: string | null;
   wallet: string;
   mailingList: boolean;
+}
+
+interface CharacterData {
+  _id: string
+  name: string
+  owner: User["_id"]
 }
 
 export interface Submission {
@@ -18,16 +27,16 @@ export interface Submission {
   voterWallets: string[];
 }
 
-const url = process.env.DB_CONN_STRING as string;
-const client = new MongoClient(url);
+const collectionNames = process.env.DB_COLLECTION?.split(',') || []
+const usersCollection = client.db(process.env.DB_NAME!).collection<User>(collectionNames[0]);
+const characterDataCollection = client.db(process.env.DB_NAME!).collection<CharacterData>(collectionNames[1]);
+const submissionCollection = client.db(process.env.DB_NAME!).collection<Submission>(collectionNames[2]);
+
 
 //function to connect to mongoDB and save a user to the database
 export async function saveUser(user: any) {
   try {
-    return client
-      .db(process.env.DB_NAME!)
-      .collection<User>(process.env.DB_COLLECTION!)
-      .insertOne(user);
+    return usersCollection.insertOne(user)
   } catch (e) {
     console.error(e);
     return e;
@@ -35,12 +44,19 @@ export async function saveUser(user: any) {
 }
 
 //function to connect to mongoDB and get a user from the database
-export async function getUser(wallet: string) {
+export async function getUserByWallet(wallet: string) {
   try {
-    return client
-      .db(process.env.DB_NAME!)
-      .collection<User>(process.env.DB_COLLECTION!)
-      .findOne({ wallet: wallet });
+    return usersCollection.findOne({ wallet: wallet })
+  } catch (error) {
+    console.error(error)
+    return error
+  }
+}
+
+//function to get user by _id
+export async function getUserById(_id: string) {
+  try {
+    return usersCollection.findOne({ _id: _id })
   } catch (error) {
     console.error(error);
     return error;
@@ -49,10 +65,7 @@ export async function getUser(wallet: string) {
 
 export async function saveSubmission(submission: Submission) {
   try {
-    return client
-      .db(process.env.DB_NAME!)
-      .collection("submissions")
-      .insertOne(submission);
+    return submissionCollection.insertOne(submission);
   } catch (error) {
     console.error(error);
     return error;
@@ -61,11 +74,7 @@ export async function saveSubmission(submission: Submission) {
 
 export async function getSubmissions() {
   try {
-    return client
-      .db(process.env.DB_NAME!)
-      .collection("submissions")
-      .find()
-      .toArray();
+    return submissionCollection.find().toArray();
   } catch (error) {
     console.error(error);
     return error;
@@ -75,9 +84,7 @@ export async function getSubmissions() {
 // returning the most liked submission
 export async function getMostLikedSubmission() {
   try {
-    return client
-      .db(process.env.DB_NAME!)
-      .collection("submissions")
+    return submissionCollection
       .find()
       .sort({ likesAmount: -1 })
       .limit(1)
@@ -95,16 +102,12 @@ export async function voteForSubmission(
 ) {
   try {
     // check if the wallet already voted
-    const submissionAlreadyVoted = await client
-      .db(process.env.DB_NAME!)
-      .collection("submissions")
+    const submissionAlreadyVoted = await submissionCollection
       .findOne({ tokenID, voterWallets: wallet });
     if (submissionAlreadyVoted) {
       return "already voted";
     }
-    return client
-      .db(process.env.DB_NAME!)
-      .collection("submissions")
+    return submissionCollection
       .updateOne(
         { tokenID },
         { $addToSet: { voterWallets: wallet }, $inc: { likesAmount: 1 } }
@@ -112,5 +115,49 @@ export async function voteForSubmission(
   } catch (error) {
     console.error(error);
     return error;
+  }
+}
+
+
+//function to list all users
+export async function getAllUsers() {
+  try {
+    const usersCursor = usersCollection.find();
+    const usersArray = await usersCursor.toArray()
+    return usersArray
+  } catch (error) {
+    throw new Error("Error retrieving users from the database");
+  }
+}
+
+//function to save character data
+export async function saveCharacterData(characterData: any, userId: string) {
+  try {
+
+    characterData.owner = getUserById(userId)
+    return characterDataCollection.insertOne(characterData)
+  } catch (error) {
+    throw new Error("Error saving character data")
+  }
+}
+
+//function to list all characters
+export async function getAllCharacters() {
+  try {
+    const characterDataCursor = characterDataCollection.find()
+    const characterDataArry = await characterDataCursor.toArray()
+    return characterDataArry
+  } catch (error) {
+    throw new Error("Error retrieving character data from the database")
+  }
+}
+
+//function to get character by id
+export async function getCharacterById(_id: string) {
+  try {
+    return characterDataCollection.findOne({ _id: _id })
+  } catch (error) {
+    console.error(error)
+    return error
   }
 }
--- frontend/utils/web3utils.js
+++ frontend/utils/web3utils.js
@@ -1,4 +1,4 @@
-import { NFTStorage, File } from 'nft.storage';
+import { NFTStorage, File } from "nft.storage";
 
 const token = process.env.NEXT_PUBLIC_NFT_STORAGE;
 
@@ -16,9 +16,9 @@ async function avatarNFTSTORAGE(someBinaryImageData, prompt, pdfData) {
 
 const base64EncodedImage = async (base64ImageString) => {
   const fetchImage = await fetch(base64ImageString);
-  console.log('fetchImage', fetchImage);
+  console.log("fetchImage", fetchImage);
   const blob = await fetchImage.blob();
-  const file = new File([blob], 'something.png', { type: 'image/png' });
+  const file = new File([blob], "something.png", { type: "image/png" });
   return file;
 };
 
@@ -33,61 +33,63 @@ async function createStructuredMetadata(pdfData, prompt, someBinaryImageData) {
 
   const imageFile = await base64EncodedImage(someBinaryImageData);
 
-  console.log('imageFile', imageFile);
+  console.log("imageFile", imageFile);
 
   const structuredMetadata = {
-    name: 'Wizard NFT!',
+    name: "Wizard NFT!",
+    likesAmount: "2",
+    tokenID: 2,
     description:
       "This is a wizard NFT, created during 'Operation Dragonborn' by our fearless heroes and the Scope Creeper! Just try to funge it. You can't do it.",
     image: imageFile,
-    external_url: 'https://operation-dragonborn.vercel.app/',
+    external_url: "https://operation-dragonborn.vercel.app/",
     attributes: [
       {
-        trait_type: 'Class',
-        value: 'Wizard',
+        trait_type: "Class",
+        value: "Wizard",
       },
       {
-        trait_type: 'Race',
-        value: 'Dragonborn!',
+        trait_type: "Race",
+        value: "Dragonborn!",
       },
       {
-        trait_type: 'Prompt',
+        trait_type: "Prompt",
         value: prompt,
       },
       {
-        trait_type: 'Level',
+        trait_type: "Level",
         value: 1,
         max_value: 20,
       },
       {
-        trait_type: 'Strength',
+        trait_type: "Strength",
         value: 5,
       },
       {
-        trait_type: 'Dexterity',
+        trait_type: "Dexterity",
         value: 5,
       },
       {
-        trait_type: 'Constitution',
+        trait_type: "Constitution",
         value: 5,
       },
       {
-        trait_type: 'Intelligence',
+        trait_type: "Intelligence",
         value: 5,
       },
       {
-        trait_type: 'Wisdom',
+        trait_type: "Wisdom",
         value: 5,
       },
       {
-        trait_type: 'Charisma',
+        trait_type: "Charisma",
         value: 5,
       },
       {
         value: pdfData.playerName ? pdfData.playerName : pdfData.name,
       },
       {
-        trait_type: 'Background',
+        trait_type: "Background",
         value: pdfData.background,
       },
     ],
