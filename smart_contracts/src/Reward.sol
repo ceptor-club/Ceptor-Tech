97a6e5563a0d610ba8f19248db9dd16e56216a5c
Emanherawy <eman.herawy@gmail.com>
2023-12-09 17:05:05 +0300

    let get winner be called with new week prompt

--- smart_contracts/src/Reward.sol
+++ smart_contracts/src/Reward.sol
@@ -2,15 +2,15 @@
 pragma solidity ^0.8.19;
 
 import {Functions, FunctionsClient} from "@chainlink/contracts/src/v0.8/dev/functions/FunctionsClient.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "./PromptCollection.sol";
+ import "./PromptCollection.sol";
 import "./CeptorDice.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
 /// @title Reward Contract
 /// @author Eman Herawy
 /// @dev Manages minting Dice NFTs for the most-liked POW NFT each week. Consumes Chainlink function to call the API and retrieve winning NFT.
 /// This contract must have the minter role in the Dices contract.
-contract Reward is Ownable, FunctionsClient {
+contract Reward is AccessControl, FunctionsClient {
     using Functions for Functions.Request;
 
     uint64 subscriptionId;
@@ -23,7 +23,8 @@ contract Reward is Ownable, FunctionsClient {
 
     event OCRResponse(bytes32 indexed requestId, bytes result, bytes err);
     event Claimed(uint amount, address by);
-
+ bytes32 public constant WINNER_MANAGEMENT_ROLE = keccak256("WINNER_MANAGEMENT_ROLE");
+ 
     /// @dev Constructor initializes the contract with the specified oracle, subscription ID, check logic, and contract addresses.
     constructor(
         address oracle,
@@ -31,16 +32,22 @@ contract Reward is Ownable, FunctionsClient {
         string memory _checkLogic,
         address diceContract,
         address promptContract
-    ) FunctionsClient(oracle) Ownable(msg.sender) {
+    ) FunctionsClient(oracle)  {
         checkLogic = _checkLogic;
         subscriptionId = _subscriptionId;
         dice = CeptorDice(diceContract);
         prompt = PromptCollection(promptContract);
+         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(WINNER_MANAGEMENT_ROLE, msg.sender);
     }
 
     /// @notice Sends a request to the Chainlink oracle to get the winner.
-    function getWinner() public {
+    function getWinner(string memory weektimeStamp) public  onlyRole(WINNER_MANAGEMENT_ROLE) {
         Functions.Request memory req;
+
+        string[] memory args = new string[](1);
+        args[0] = weektimeStamp;
+        req.addArgs(args);
         req.initializeRequest(Functions.Location.Inline, Functions.CodeLanguage.JavaScript, checkLogic);
         bytes32 assignedReqID = sendRequest(req, subscriptionId, 300000);
     }
@@ -65,10 +72,10 @@ contract Reward is Ownable, FunctionsClient {
         _ids[3] = 3;
 
         uint256[] memory _amounts = new uint256[](4);
-        _amounts[0] = 1;
-        _amounts[1] = 1;
-        _amounts[2] = 1;
-        _amounts[3] = 1;
+        _amounts[0] = 5;
+        _amounts[1] = 5;
+        _amounts[2] = 5;
+        _amounts[3] = 5;
 
         // Mint NFTs using the Dices contract
         dice.minterMintBatch(_to, _ids, _amounts, "");
@@ -81,7 +88,7 @@ contract Reward is Ownable, FunctionsClient {
 
     /// @notice Allows the Functions oracle address to be updated.
     /// @param oracle New oracle address.
-    function updateOracleAddress(address oracle) public onlyOwner {
+    function updateOracleAddress(address oracle) public onlyRole(AccessControl.DEFAULT_ADMIN_ROLE) {
         setOracle(oracle);
     }
 }
