a6ad255a4737cd7309a661d940eae7d53840ca88
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-12-10 23:17:49 +0100

    fix: alignmentGood page ui fix

--- frontend/components/QuizComponents/AlignmentGoodEvil.jsx
+++ frontend/components/QuizComponents/AlignmentGoodEvil.jsx
@@ -10,9 +10,9 @@ export default function AlignmentGoodEvil({ setMyAlignment, myAlignment }) {
         }
     }
     return (
-        <div >
-            <h3 className="text-3xl">Which quest seems the most worthy of your time?</h3>
-            <div className='flex flex-col mt-8 text-xl space-y-8 > * + *' >
+        <div>
+            <h3 className="text-3xl text-white p-3 mb-8">Which quest seems the most worthy of your time?</h3>
+            <div className='flex flex-col text-xl justify-between gap-6 mb-20 text-white' >
                 <div>
                     <input
                         type="radio"
@@ -20,11 +20,11 @@ export default function AlignmentGoodEvil({ setMyAlignment, myAlignment }) {
                         id="Good"
                         checked={myAlignment === "Good" || myAlignment === "Chaotic Good" || myAlignment === "Nuetral Good" || myAlignment === "Lawful Good"}
                         onChange={(e) => handleAlignmentChange(e.target.value)}
-                        className="hidden"
+                        className="mr-4 w-6 h-6 align-middle accent-customYellow"
                     />
                     <label
                         htmlFor="Good"
-                        className={`cursor-pointer p-2 rounded-md ${myAlignment === "Good" || myAlignment === "Chaotic Good" || myAlignment === "Nuetral Good" || myAlignment === "Lawful Good" ? "border-4 border-solid border-ceptor p-2" : ""
+                        className={`cursor-pointer p-2 rounded-md ${myAlignment === "Good" || myAlignment === "Chaotic Good" || myAlignment === "Nuetral Good" || myAlignment === "Lawful Good" ? "border-2 border-solid border-customYellow p-2" : ""
                             }`}
                     >
                         Defending helpless villagers against a group of cruel raiders.
@@ -38,11 +38,11 @@ export default function AlignmentGoodEvil({ setMyAlignment, myAlignment }) {
                         id="Neutral"
                         checked={myAlignment === "Chaotic Neutral" || myAlignment === "Nuetral Nuetral" || myAlignment === "Lawful Neutral"}
                         onChange={(e) => handleAlignmentChange(e.target.value)}
-                        className="hidden"
+                        className="mr-4 w-6 h-6 align-middle accent-customYellow"
                     />
                     <label
                         htmlFor="Neutral"
-                        className={`cursor-pointer p-2 rounded-md ${myAlignment === "Chaotic" || myAlignment === "Lawful" || myAlignment === "Neutral" || myAlignment === "Chaotic" || myAlignment === "Chaotic Neutral" || myAlignment === "Nuetral Nuetral" || myAlignment === "Lawful Neutral" ? "border-4 border-solid border-ceptor p-2" : ""
+                        className={`cursor-pointer p-2 rounded-md ${myAlignment === "Chaotic" || myAlignment === "Lawful" || myAlignment === "Neutral" || myAlignment === "Chaotic" || myAlignment === "Chaotic Neutral" || myAlignment === "Nuetral Nuetral" || myAlignment === "Lawful Neutral" ? "border-2 border-solid border-customYellow p-2" : ""
                             }`}
                     >
                         Slaying a vicious ogre and taking his gold.
@@ -56,11 +56,11 @@ export default function AlignmentGoodEvil({ setMyAlignment, myAlignment }) {
                         id="Evil"
                         checked={myAlignment === "Evil" || myAlignment === "Chaotic Evil" || myAlignment === "Nuetral Evil" || myAlignment === "Lawful Evil"}
                         onChange={(e) => handleAlignmentChange(e.target.value)}
-                        className="hidden"
+                        className="mr-4 w-6 h-6 align-middle accent-customYellow"
                     />
                     <label
                         htmlFor="Evil"
-                        className={`cursor-pointer p-2 rounded-md ${myAlignment === "Evil" || myAlignment === "Chaotic Evil" || myAlignment === "Nuetral Evil" || myAlignment === "Lawful Evil" ? "border-4 border-solid border-ceptor p-2" : ""
+                        className={`cursor-pointer p-2 rounded-md ${myAlignment === "Evil" || myAlignment === "Chaotic Evil" || myAlignment === "Nuetral Evil" || myAlignment === "Lawful Evil" ? "border-2 border-solid border-customYellow p-2" : ""
                             }`}
                     >
                         Recovering a cursed ring that will grant you the power to enslave others.
