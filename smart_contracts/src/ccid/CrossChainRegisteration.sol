4ab958b84cb4d35ba72832f09d1b12dec3eab442
Emanherawy <eman.herawy@gmail.com>
2023-12-10 03:36:19 +0300

    ccip receive from 2 chains is supported now

--- smart_contracts/src/ccid/CrossChainRegisteration.sol
+++ smart_contracts/src/ccid/CrossChainRegisteration.sol
@@ -6,8 +6,19 @@ import "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 abstract contract CrossChainRegisteration is CCIPReceiver, Ownable{
+    //  uint64 constant chainIdEthereumSepolia = 16015286601757825753;
+     uint64 constant chainIdAvalancheFuji = 14767482510784806043;
+     uint64 constant chainIdPolygonMumbai = 12532609583862916517;
   // approved list of sender addresses for CCIP messages
     mapping(address => bool) public approvedSenders;
+
+
+     
+    modifier onlyAllowedSenders(address _sender) {
+        if (! approvedSenders[_sender])
+            revert SenderNotAllowed(_sender);
+        _;
+    }
     constructor(address router) CCIPReceiver(router) Ownable(msg.sender) {}
 
       // function to let the owner set the approved senders
@@ -15,6 +26,16 @@ abstract contract CrossChainRegisteration is CCIPReceiver, Ownable{
         approvedSenders[_sender] = _approved;
     }
 
+    // Event emitted when a message is received from another chain.
+    event MessageReceived(
+        bytes32 indexed messageId, // The unique ID of the CCIP message.
+        uint64 indexed sourceChainSelector, // The chain selector of the source chain.
+        address sender, // The address of the sender from the source chain.
+        string text, // The text that was received.
+        address token, // The token address that was transferred.
+        uint256 tokenAmount // The token amount that was transferred.
+    );
+error SenderNotAllowed(address sender);
+    error DestinationChainNotAllowlisted(uint64 destinationChainSelector); // Used when the destination chain has not been allowlisted by the contract owner.
 
-    
 }
