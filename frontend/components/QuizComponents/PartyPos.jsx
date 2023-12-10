a15637fb85659c15132de6a07e8f52ebbd0c7e25
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-12-10 23:12:16 +0100

    fix: partyPos page UI fix

--- frontend/components/QuizComponents/PartyPos.jsx
+++ frontend/components/QuizComponents/PartyPos.jsx
@@ -1,9 +1,10 @@
 export default function PartyPos({ partyPos, setPartyPos }) {
     return (
-        <div >
-            <h3 className="text-3xl mb-4">You and your party ambush a group of monsters.</h3>
-            <h3 className="text-3xl">Where do you see yourself in this fight?</h3>
-            <div className='flex flex-col mt-12 text-xl space-y-8 > * + *' >
+        <div>
+            <div className="text-white p-3 mb-8">
+                <h3 className="text-3xl">You and your party ambush a group of monsters. <br /> Where do you see yourself in this fight?</h3>
+            </div>
+            <div className='flex flex-col text-xl justify-between gap-6 mb-20 text-white' >
                 <div>
                     <input
                         type="radio"
@@ -11,11 +12,11 @@ export default function PartyPos({ partyPos, setPartyPos }) {
                         id="partyRole1Front"
                         checked={partyPos === "front"}
                         onChange={(e) => setPartyPos(e.target.value)}
-                        className="hidden"
+                        className="mr-4 w-6 h-6 align-middle accent-customYellow"
                     />
                     <label
                         htmlFor="partyRole1Front"
-                        className={`cursor-pointer p-2 rounded-md ${partyPos === "front" ? "border-4 border-solid border-ceptor p-2" : ""
+                        className={`cursor-pointer p-2 rounded-md ${partyPos === "front" ? "border-4 border-solid border-customYellow p-2" : ""
                             }`}
                     >
                         In the front leading the charge
@@ -29,11 +30,11 @@ export default function PartyPos({ partyPos, setPartyPos }) {
                         id="partyRole1Middle"
                         checked={partyPos === "middle"}
                         onChange={(e) => setPartyPos(e.target.value)}
-                        className="hidden"
+                        className="mr-4 w-6 h-6 align-middle accent-customYellow"
                     />
                     <label
                         htmlFor="partyRole1Middle"
-                        className={`cursor-pointer p-2 rounded-md ${partyPos === "middle" ? "border-4 border-solid border-ceptor p-2" : ""
+                        className={`cursor-pointer p-2 rounded-md ${partyPos === "middle" ? "border-4 border-solid border-customYellow p-2" : ""
                             }`}
                     >
                         In the middle launching ranged attacks
@@ -47,11 +48,11 @@ export default function PartyPos({ partyPos, setPartyPos }) {
                         id="partyRole1Back"
                         checked={partyPos === "back"}
                         onChange={(e) => setPartyPos(e.target.value)}
-                        className="hidden"
+                        className="mr-4 w-6 h-6 align-middle accent-customYellow"
                     />
                     <label
                         htmlFor="partyRole1Back"
-                        className={`cursor-pointer p-2 rounded-md ${partyPos === "back" ? "border-4 border-solid border-ceptor p-2" : ""
+                        className={`cursor-pointer p-2 rounded-md ${partyPos === "back" ? "border-4 border-solid border-customYellow p-2" : ""
                             }`}
                     >
                         In the back supporting your team
