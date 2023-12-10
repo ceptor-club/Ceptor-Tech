8bf0f45148e1b75849cd9708ba60af9f0f1e4c28
Emanherawy <eman.herawy@gmail.com>
2023-12-10 03:36:19 +0300

    ccip receive from 2 chains is supported now

--- smart_contracts/src/CeptorCCID.sol
+++ smart_contracts/src/CeptorCCID.sol
@@ -52,42 +52,60 @@ contract CeptorCCID  is PriceFeedCCID, CrossChainRegisteration {
 
 
 
-        // Function to update user stats
-    function updateBigStats(uint256 statId, uint256 value) public onlyOwner {
-        // if (statId == 0) userStats[msg.sender].strong = uint8(value);
-        // else if (statId == 1) userStats[msg.sender].agile = uint8(value);
-        // else if (statId == 2) userStats[msg.sender].tanky = uint8(value);
-        // else if (statId == 3) userStats[msg.sender].clever = uint8(value);
-        // else if (statId == 4) userStats[msg.sender].wise = uint8(value);
-        // else if (statId == 5) userStats[msg.sender].cute = uint8(value);
-        // else if (statId == 6) userStats[msg.sender].lucky = uint8(value);
-        emit StatsUpdated(msg.sender, statId, value);
+        // Function to update user stats, only called _ccipReceive
+    function _updateBigStats(uint256 statId, uint256 value, address _user) internal {
+       
+        if (statId == 0) users[_user].stats.strong = uint8(value);
+        else if (statId == 1) users[_user].stats.agile = uint8(value);
+        else if (statId == 2) users[_user].stats.tanky = uint8(value);
+        else if (statId == 3) users[_user].stats.clever = uint8(value);
+        else if (statId == 4) users[_user].stats.wise = uint8(value);
+        else if (statId == 5) users[_user].stats.cute = uint8(value);
+        else if (statId == 6) users[_user].stats.lucky = uint8(value);
+         emit StatsUpdated(msg.sender, statId, value);
     }
 
-    // Function to store loot
-    function storeLoot(string memory loot) public onlyOwner {
-        // for (uint256 i = 0; i < amount; i++) {
-        //     //userLoot[msg.sender].push(loot);
-        //     emit LootReceived(msg.sender, loot);
-        // }
+    // Function to store loot. Only called by _ccipReceive
+    function _storeLoot(string memory loot, address user) internal  {
+        require(bytes(users[user].username).length != 0, "User must have a username");
+        users[user].loot.push(loot);
+        emit LootReceived(user, loot);
     }
 
     // CCIP Receive function -- have a seperate contract for each recieve (Loot and Stats)
     
-    function _ccipReceive(Client.Any2EVMMessage memory any2EvmMessage) internal override {
-        // // Does this work? @Chainlink Dev Experts
-        // require(approvedSenders[abi.decode(any2EvmMessage.sender, (address))], "Sender not approved");
-        
-        // // Example: Update stats or perform other actions based on received data
-        // // Not sure about how we can have security based on making sure that the Loot and Stats are from the right contract
-        // // I guess we can have an allowlist of contracts that can send data to this contract
-        // // And depending on the structure of the message we can update the stats or store the loot
-        // // Extract the user's address and loot from the message
-        // (address userAddress, string memory loot) = abi.decode(any2EvmMessage.data, (address, string));
-        // // Store the loot for the user
-        // require(bytes(users[userAddress].username).length != 0, "User must have a username");
-        // users[userAddress].loot.push(loot);
-        // emit LootReceived(userAddress, loot);
+    function _ccipReceive(Client.Any2EVMMessage memory any2EvmMessage) internal override onlyAllowedSenders(abi.decode(any2EvmMessage.sender, (address))) {
+     
+        if ( any2EvmMessage.sourceChainSelector == chainIdAvalancheFuji) {
+            // Example: Update stats or perform other actions based on received data
+            // Not sure about how we can have security based on making sure that the Loot and Stats are from the right contract
+            // I guess we can have an allowlist of contracts that can send data to this contract
+            // And depending on the structure of the message we can update the stats or store the loot
+            // Extract the user's address and loot from the message
+            (address userAddress, string memory loot) = abi.decode(any2EvmMessage.data, (address, string));
+            // Store the loot for the user
+            if (bytes(users[userAddress].username).length == 0) {
+                revert UserIsNotRegistered(userAddress);
+            }
+             users[userAddress].loot.push(loot);
+            emit LootReceived(userAddress, loot);
+        } else if (any2EvmMessage.sourceChainSelector == chainIdPolygonMumbai) {
+            // Example: Update stats or perform other actions based on received data
+            // Not sure about how we can have security based on making sure that the Loot and Stats are from the right contract
+            // I guess we can have an allowlist of contracts that can send data to this contract
+            // And depending on the structure of the message we can update the stats or store the loot
+            // Extract the user's address and loot from the message
+            (address userAddress, uint256 statId, uint256 value) = abi.decode(any2EvmMessage.data, (address, uint256, uint256));
+            // Store the loot for the user
+              if (bytes(users[userAddress].username).length == 0) {
+                revert UserIsNotRegistered(userAddress);
+            }
+            _updateBigStats(statId, value, userAddress);
+        } else {
+            revert DestinationChainNotAllowlisted(any2EvmMessage.sourceChainSelector );
+        }
+     
+     
     }
 
 
@@ -96,4 +114,11 @@ contract CeptorCCID  is PriceFeedCCID, CrossChainRegisteration {
     event UsernameRegistered(address indexed user, string username, bool isFree);
     event StatsUpdated(address indexed user, uint256 statId, uint256 value);
     event LootReceived(address indexed user, string loot);
+
+
+    // errors
+    error UsernameAlreadySet(string username);
+    error UsernameNotSet(address user);
+    error StatIdOutOfRange(uint256 statId);
+    error UserIsNotRegistered(address user);
 }
