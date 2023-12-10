c16d312ea6f6d23cc17763fda95a384f6181d5ec
Emanherawy <eman.herawy@gmail.com>
2023-12-10 04:48:13 +0300

    add deployment script

---
+++ smart_contracts/script/deployer2.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "forge-std/Script.sol";
+import "./Helper.sol";
+// import {CeptorDice} from  "../src/CeptorDice.sol";
+// import {Ceptors} from "../src/Ceptors.sol";
+// import "../src/PromptCollection.sol";
+// import "../src/Reward.sol";
+import "../src/CeptorCCID.sol";
+// import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+// import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+// import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.0/token/ERC20/IERC20.sol";
+
+contract Deployer is Script, Helper {
+    function deployAll(  SupportedNetworks destination, address dice
+    ) external  {
+
+        
+     
+        uint256 senderPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(senderPrivateKey);
+    
+        (address desinationRouter, , ,, address priceFeed  ) = getConfigFromNetwork(destination);
+        // deploy the CeptorCCID contract
+        CeptorCCID ccid = new CeptorCCID( dice, desinationRouter, priceFeed);
+        vm.stopBroadcast();
+         
+    }
+  
+
+ 
+
+}
