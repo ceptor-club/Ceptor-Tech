b5a878f217c516d92602f1901050e3718cbc5dee
Lena Hierzi <lenahierzi@hotmail.com>
2023-12-10 13:25:11 +0000

    style NFT Modal and add new import structure

--- frontend/components/NftModal.tsx
+++ frontend/components/NftModal.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import Image from "next/image";
+import { SubmitData } from "../utils/types";
 
-const NftModal = ({ modalOpen, setModalOpen, nft }) => {
+const NftModal = ({
+  modalOpen,
+  setModalOpen,
+  nft,
+}: {
+  modalOpen: boolean;
+  setModalOpen: any;
+  nft: SubmitData;
+}) => {
   const closeModal = (e) => {
     const search = e.target.className;
     if (search.includes("modal")) {
@@ -16,7 +25,7 @@ const NftModal = ({ modalOpen, setModalOpen, nft }) => {
           className="modal fixed top-0 left-0 w-full h-full z-50  bg-opacity-80 flex items-center justify-center text-white"
           onClick={closeModal}
         >
-          <div className="scrollableDiv p-6 pt-2 bg-black border text-center rounded-lg flex flex-col items-center max-w-md mx-auto">
+          <div className="font-oswald scrollableDiv p-6 pt-2 bg-black border text-center rounded-lg flex flex-col items-center max-w-md mx-auto">
             {" "}
             {/* Changed max-w-lg to max-w-md */}
             <p className="w-full text-right text-4xl">
@@ -24,22 +33,37 @@ const NftModal = ({ modalOpen, setModalOpen, nft }) => {
                 &times;
               </b>
             </p>
-            <div className="w-full flex flex-col items-center">
-              <h2 className="text-2xl">{nft.rawMetadata.name}</h2>
-              <div className="flex justify-center">
+            <div className="w-full flex flex-col items-center space-y-3 ">
+              <div className="flex justify-center rounded-lg">
                 <Image
-                  src={nft.rawMetadata.image.replace(
-                    "ipfs://",
-                    "https://ipfs.io/ipfs/"
-                  )}
-                  alt={nft.rawMetadata.name}
+                  src={nft.image}
+                  alt={nft.addressOfCreator || ""}
                   width={350}
                   height={350}
                 />{" "}
                 {/* Reduced image size */}
               </div>
-              <p>{nft.rawMetadata.description}</p>
-              <ul className="w-full">
+              <p className="font-milonga text-light-yellow text-sm max-w-md overflow-auto w-2/3">
+                Creator:
+              </p>
+              <p className="text-xs max-w-md overflow-auto w-2/3">
+                {nft.addressOfCreator}
+              </p>
+              <p className="font-milonga text-sm text-light-yellow max-w-md overflow-hidden w-2/3">
+                Voter:
+              </p>
+              <div className="text- max-w-md overflow-hidden w-2/3">
+                {nft.voterWallets.map((item) => (
+                  <div
+                    className="flex flex-row justify-center items-center  "
+                    key={item}
+                  >
+                    {/* Accessing properties from each item */}
+                    <p className="text-xs max-w-md overflow-auto">{item}</p>
+                  </div>
+                ))}
+              </div>
+              {/* <ul className="w-full">
                 {nft.rawMetadata.attributes
                   .filter((attr) => attr.trait_type !== "Prompt")
                   .map((attr, index) => (
@@ -63,7 +87,7 @@ const NftModal = ({ modalOpen, setModalOpen, nft }) => {
                       <span>{attr.value}</span>
                     </li>
                   ))}
-              </ul>
+              </ul> */}
             </div>
           </div>
         </div>
